16a1602c6ebd4a53141f499e3fa3dbadad6e88f1
az204-elicrisko <109534368+az204-elicrisko@users.noreply.github.com>
2022-08-05 07:58:29 -0700

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello Spring Boot App Service JavaSE World";
 	}
 
 	public static void main(String[] args) {
